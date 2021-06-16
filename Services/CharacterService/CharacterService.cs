36b963101c7a996f64f435d3159a5fa130f7283c
brunaoliveira <Bruna_Oliveira1@Dellteam.com>
2021-06-16 13:19:24 -0300

    Adding automapper

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Models;
 
-namespace dotnet_rpg.Services.CharacterService 
+namespace dotnet_rpg.Services.CharacterService
 {
   public class CharacterService : ICharacterService
   {
@@ -12,12 +13,18 @@ namespace dotnet_rpg.Services.CharacterService
       new Character(),
       new Character{ Id = 1, Name = "Chico" }
     };
+    private readonly IMapper _mapper;
+
+    public CharacterService(IMapper mapper)
+    {
+      _mapper = mapper;
+    }
 
     public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
     {
       var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-      characters.Add(newCharacter);
-      serviceResponse.Data = characters;
+      characters.Add(_mapper.Map<Character>(newCharacter));
+      serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
 
       return serviceResponse;
     }
@@ -25,7 +32,7 @@ namespace dotnet_rpg.Services.CharacterService
     public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
     {
       var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-      serviceResponse.Data = characters;
+      serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
 
       return serviceResponse;
     }
@@ -33,7 +40,7 @@ namespace dotnet_rpg.Services.CharacterService
     public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
     {
       var serviceResponse = new ServiceResponse<GetCharacterDto>();
-      serviceResponse.Data = characters.FirstOrDefault(c => c.Id == id);
+      serviceResponse.Data = _mapper.Map<GetCharacterDto>(characters.FirstOrDefault(c => c.Id == id));
 
       return serviceResponse;
     }
