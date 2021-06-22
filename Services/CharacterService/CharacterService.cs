48cb7b0bb045ced12b382d634f7f4bba2256bd0d
brunaoliveira <Bruna_Oliveira1@Dellteam.com>
2021-06-22 19:43:21 -0300

    Treat the updateCharacter method for exceptions

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -51,16 +52,23 @@ namespace dotnet_rpg.Services.CharacterService
     public async Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter)
     {
       var serviceResponse = new ServiceResponse<GetCharacterDto>();
-      Character character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
       
-      character.Name = updatedCharacter.Name;
-      character.HitPoints = updatedCharacter.HitPoints;
-      character.Strength = updatedCharacter.Strength;
-      character.Defense = updatedCharacter.Defense;
-      character.Intelligence = updatedCharacter.Intelligence;
-      character.Class = updatedCharacter.Class;
+      try {
+        Character character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
+        character.Name = updatedCharacter.Name;
+        character.HitPoints = updatedCharacter.HitPoints;
+        character.Strength = updatedCharacter.Strength;
+        character.Defense = updatedCharacter.Defense;
+        character.Intelligence = updatedCharacter.Intelligence;
+        character.Class = updatedCharacter.Class;
 
-      serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+        serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+      } 
+      catch(Exception ex)
+      {
+        serviceResponse.Success = false;
+        serviceResponse.Message = ex.Message;
+      }
 
       return serviceResponse;
     }
