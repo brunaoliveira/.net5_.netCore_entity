ccca3a2272f95646d50729167aca04ba8b83c9b5
brunaoliveira <Bruna_Oliveira1@Dellteam.com>
2021-07-14 13:12:24 -0300

    Add migration: weapons table added, with FK CharacterId

--- Models/Weapon.cs
+++ Models/Weapon.cs
@@ -6,5 +6,6 @@ namespace dotnet_rpg.Models
     public string Name { get; set; }
     public int Damage { get; set; }
     public Character Character { get; set; }
+    public int CharacterId  { get; set; }
   }
 }
