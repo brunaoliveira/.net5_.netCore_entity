4d6cc0386c916b83df42930d8dac22f2c5f7e708
brunaoliveira <Bruna_Oliveira1@Dellteam.com>
2021-07-16 11:16:24 -0300

    Migration: create Skills table

--- Models/Skill.cs
+++ Models/Skill.cs
@@ -2,11 +2,11 @@ using System.Collections.Generic;
 
 namespace dotnet_rpg.Models
 {
-    public class Skill
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Damage { get; set; }
-        public List<Character> Characters { get; set; }
-    }
+  public class Skill
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Damage { get; set; }
+    public List<Character> Characters { get; set; }
+  }
 }
